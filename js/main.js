0d8b87148132d41d4d7ac96e9ff5c390336a6aa0
gangya0327 <king9999427@163.com>
2018-02-28 17:36:05 +0800

    narrow-header

--- js/main.js
+++ js/main.js
@@ -1,27 +1,11 @@
-/*const Foo = {template: '<div>foo</div>'}
-const Bar = {template: '<div>bar</div>'}
-
-
-const routes = [
-	{path: '/foo', component: Foo},
-	{path: '/bar', component: Bar}
-]
-
-const router = new VueRouter({
-	routes//相当于routes: routes
-})*/
-
-//动态路由
-const User = {
-	template: `<div>User {{ $route.params.id }}</div>`
-}
-
-const router = new VueRouter({
-	routes:[
-	{ path: '/user/:id', component: User }
-	]
+$(document).ready(function(){
+	$(window).scroll(function(){
+		var scrollPos = $(window).scrollTop();
+		if(scrollPos > 0){
+			$("header").addClass("narrow-header")
+		}
+		if(scrollPos == 0){
+			$("header").removeClass("narrow-header");
+		}
+	})
 })
-
-const app = new Vue({
-	router
-}).$mount('#app')

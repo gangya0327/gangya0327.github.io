e19bd20e78a22d4c636f4f6ea14c0d7da1aea4e2
gangya0327 <king9999427@163.com>
2018-03-29 17:38:15 +0800

    mini

---
+++ page/test/MiniProgram/client/pages/test/test2.js
@@ -0,0 +1,17 @@
+// pages/test/test2.js
+Page({
+  data: {
+    staffA: {firstname: 'kilo', lastname: 'pp'},
+    array: [15,22,34,42,5,6]
+  },
+  showInfo: function (event){
+    console.log(event)
+  },
+  longShowInfo: function(){
+    console.log('longShowInfo')
+  }
+
+
+
+
+})
